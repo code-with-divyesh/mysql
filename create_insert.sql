e85865c6415d925569e55013dd3bcb69aabb54ee
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-12-05 16:18:25 +0530

    insert a row in dept table

--- create_insert.sql
+++ create_insert.sql
@@ -14,3 +14,5 @@ insert into DEPARTMENT values(40, 'Sales',"sales01",01, 'NY');
  insert into DEPARTMENT values(50, 'EDP',"edp01",01, 'MU');
  insert into DEPARTMENT values(60, 'TRG',"trg01",01, 'rj');
 insert into DEPARTMENT values(110, 'RND',"rnd01",01, 'AH'); 
+insert into DEPARTMENT values(130, 'finance',"fin01",01, 'mp');
+insert into DEPARTMENT values(140, 'purchase',"pur01",01, null);
