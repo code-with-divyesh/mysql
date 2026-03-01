2bc93cfb2ea7ff1cb272d93d5ecb6a80522b18ba
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2026-03-01 21:54:27 +0530

    feat: add Coffee Sales Analytics SQL project with business KPIs

---
+++ projects/08_Monday_coffee_Analysis/08_Monday_coffee_Analysis.sql
@@ -0,0 +1,45 @@
+create database coffee_db;
+use coffee_db;
+
+-- Drop Tables
+DROP TABLE IF EXISTS sales;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS city;
+
+-- City Table
+CREATE TABLE city (
+    city_id INT PRIMARY KEY,
+    city_name VARCHAR(15),
+    population BIGINT,
+    estimated_rent FLOAT,
+    city_rank INT
+);
+
+-- Products Table
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(35),
+    price FLOAT
+);
+
+-- Customers Table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(25),
+    city_id INT,
+    FOREIGN KEY (city_id) REFERENCES city(city_id)
+);
+
+-- Sales Table
+CREATE TABLE sales (
+    sale_id INT PRIMARY KEY,
+    sale_date DATE,
+    product_id INT,
+    customer_id INT,
+    total FLOAT,
+    rating INT,
+    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
